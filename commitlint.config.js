98521d76c2cc0f1d3a243667c3e4de4969f0c057
廖建国 <1742992503@qq.com>
2023-07-24 09:08:19 +0800

    提交commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert']],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0],
+    'subject-empty': [0],
+    'subject-case': [0],
+    'header-max-length': [0]
+  }
+}
